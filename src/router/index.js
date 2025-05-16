dd44f6cf28a75fd483dd036ccdbfd8f8f990b666
Kyle Beachill <kyle@beachill.co.uk>
2025-05-16 16:31:35 +0100

    general fixes after cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DefaultView from '../views/DefaultView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,26 +19,10 @@ const router = createRouter({
       props: true,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/update/:id',
       name: 'update',
       component: () => import('../views/IDMainView.vue'),
       props: true,
-    },
-    {
-      path: '/create',
-      name: 'create',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CreateView.vue'),
     },
   ],
 })
--- src/views/DefaultView.vue
+++ src/views/DefaultView.vue
@@ -1,12 +1,12 @@
 <script setup>
-import BaseFullpageLayout from '../components/BaseFullpageLayout.vue'
+
 </script>
 
 <template>
-  <BaseFullpageLayout>
+  <div class="bg-wc kyle flex min-h-full flex-col">
     <!--<div class="mx-auto flex flex-col lg:flex-row w-full items-stretch px-6 md:px-0">-->
     <div>
       <slot></slot>
     </div>
-  </BaseFullpageLayout>
+  </div>
 </template>
